3684c4eac0bfd3f7a2de5dc6589f404a60e3ba6c
Pavel Irinarkhov <magicringltd@gmail.com>
2023-06-09 21:56:29 +0600

    modified services

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -41,9 +41,10 @@ public class StatsService {
 
     public int getMonthsUnderAverage(long[] sales) {
         int monthsUnderAverage = 0;
+        long salesAverage = getSalesAverage(sales);
 
         for (long sale : sales) {
-            if (sale < getSalesAverage(sales)) {
+            if (sale < salesAverage) {
                 monthsUnderAverage++;
             }
         }
@@ -52,9 +53,10 @@ public class StatsService {
 
     public int getMonthsOverAverage(long[] sales) {
         int monthsOverAverage = 0;
+        long salesAverage = getSalesAverage(sales);
 
         for (long sale : sales) {
-            if (sale < getSalesAverage(sales)) {
+            if (sale < salesAverage) {
                 monthsOverAverage++;
             }
         }
